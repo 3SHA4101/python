ef151932d926e110f37c2ed0208fbe09443e581e
3SHA4101 <thrisha4101@gmail.com>
2025-03-10 15:05:38 +0530

    Create arithmatic.py

---
+++ arithmatic.py
@@ -0,0 +1,8 @@
+a=int((input("enter a:"))
+b=int((input("enter b:"))
+print("multiplication result=",int(a*b)
+print("adition=",a+b)
+print("substraction=",a-b)
+print("division=",a/b)
+print("division=",a//b)#gives whole quotient
+print("power result=",(a*b)
