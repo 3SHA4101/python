12868c2bbd426d2370a2741368fe626189a2da49
3SHA4101 <thrisha4101@gmail.com>
2025-03-10 13:03:37 +0530

    Create oddoreven.py

---
+++ oddoreven.py
@@ -0,0 +1,9 @@
+def check_odd_even(number):
+    if number % 2 == 0:
+        print(f"{number} is even.")
+    else:
+        print(f"{number} is odd.")
+
+# Example calls
+check_odd_even(4)  
+check_odd_even(7)  
