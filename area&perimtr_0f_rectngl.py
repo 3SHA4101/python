1cddc2ecf7899d5464a2f55716a658ab5c56e4cc
3SHA4101 <thrisha4101@gmail.com>
2025-03-10 16:17:35 +0530

    Create area&perimtr_0f_rectngl.py

---
+++ area&perimtr_0f_rectngl.py
@@ -0,0 +1,10 @@
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+# Calculate area and perimeter
+area = length * width
+perimeter = 2 * (length + width)
+
+# Display results
+print("Area of the rectangle:",area)
+print("Perimeter of the rectangle: ",perimeter)
