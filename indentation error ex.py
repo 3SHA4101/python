0003928c397d1d43f94823ed1435dbe8daaee4dc
3SHA4101 <thrisha4101@gmail.com>
2025-03-10 12:03:38 +0530

    Create indentation error ex.py

---
+++ indentation error ex.py
@@ -0,0 +1,21 @@
+def greet(name):
+    if name:
+        print(f"Hello, {name}!")
+      print("This line is misaligned")  # Indentation error
+
+greet("Alice")
+
+
+
+
+
+
+
+
+
+//code o/p error is    File "<tokenize>", line 4
+    print("This line is misaligned")  # Indentation error
+    ^
+IndentationError: unindent does not match any outer indentation level
+
+
