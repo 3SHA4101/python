f3a6e74a032d43cb0490a09d79a82a43a5868ddb
3SHA4101 <thrisha4101@gmail.com>
2025-03-12 07:32:43 +0530

    Create square_of_nno.py

---
+++ square_of_nno.py
@@ -0,0 +1,6 @@
+def sum_of_squares(n):
+    return (n * (n + 1) * (2 * n + 1)) // 6
+
+n = int(input("Enter a number: "))
+print("3sha-31")
+print(f"Sum of squares of first {n} numbers is {sum_of_squares(n)}")
