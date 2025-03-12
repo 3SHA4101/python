79fdf472e3b5f2bef070702d4df3bedf6bc7b7bb
3SHA4101 <thrisha4101@gmail.com>
2025-03-12 19:24:35 +0530

    Create func_lcm.py

---
+++ func_lcm.py
@@ -0,0 +1,8 @@
+def lcm(a,b):
+  import math
+  a = int(input("Enter first number: "))
+  b = int(input("Enter second number: "))
+  lcm = abs(a * b) // math.gcd(a, b)
+  print("3sha 31")
+  print(f"LCM of {a} and {b} is {lcm}")
+  
