ff80930d0eb5e67f598e6daae63d3b9619573a42
3SHA4101 <thrisha4101@gmail.com>
2025-03-12 22:23:19 +0530

    Create sorted_data.py

---
+++ sorted_data.py
@@ -0,0 +1,8 @@
+data = [(1, 3), (4, 1), (2, 2), (5, 0)]
+
+# Sorting based on the second element of each tuple
+sorted_data = sorted(data, key=lambda x: x[1])
+
+# Output result
+print(sorted_data)  # Output: [(5, 0), (4, 1), (2, 2), (1, 3)]
+print("3sha 31")
