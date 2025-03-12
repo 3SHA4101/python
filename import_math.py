0a7c321365247837fa980e0ae58ed1372ed1e99d
3SHA4101 <thrisha4101@gmail.com>
2025-03-12 21:04:00 +0530

    Create import_math.py

---
+++ import_math.py
@@ -0,0 +1,10 @@
+import math
+from math import sqrt,sin,factorial
+n=int(input("entera a number"))
+sqrt=math.sqrt(n)
+fact=math.factorial(n)
+sin=math.sin(n)
+print(f"square root of {n} is {sqrt}")
+print(f"factorial of {n} is {fact}")
+print(f"sin angle of {n} is {sin}")
+print("3sha 31")
