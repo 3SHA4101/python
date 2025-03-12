faaf16a8c96680f09f1a87de5f25ba25b6f27d4e
3SHA4101 <thrisha4101@gmail.com>
2025-03-12 15:30:16 +0530

    Create func_calci.py

---
+++ func_calci.py
@@ -0,0 +1,14 @@
+def calci():
+  a=int(input("enter 1st number "))
+  b=int(input("enter 1st number "))
+  c=int(input("choose 1=+, 2=-, 3=*,4=/"))
+  if c==1 :
+    print(f"addition result ={a+b}")
+  elif c==2 :
+    print(f"addition result ={a-b}")
+  elif c==3 :
+    print(f"addition result ={a*b}")
+  elif c==4 :
+    print(f"addition result ={a/b}")
+  else:
+    print("invalid choose")
