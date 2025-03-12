5f7a19aa1d00b7c10301b6f9b45b09814be7567a
3SHA4101 <thrisha4101@gmail.com>
2025-03-12 12:01:47 +0530

    Update fun_prime_no.py

--- fun_prime_no.py
+++ fun_prime_no.py
@@ -5,7 +5,7 @@ def is_prime(a):
       if a % i == 0:
           print(f"{a} is not a prime number.")
           break
-   else:
-      print(f"{a} is a prime number.")
+      else:
+          print(f"{a} is a prime number.")
   else:
     print(f"{a} is not a prime number.")
